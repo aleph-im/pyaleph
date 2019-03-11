75a8de25567baabd8f9d7eba4e55371631ee3268
moshe malawach <moshe.malawach@protonmail.com>
2019-03-11 17:05:46 +0100

    empty main

--- src/aleph/web/main.py
+++ src/aleph/web/main.py
@@ -4,25 +4,17 @@ import aiohttp_jinja2
 from aiocache import cached, SimpleMemoryCache
 from aiohttp import web, ClientSession
 
-from nulsexplorer.main import api_post, request_block
-from nulsexplorer.web import app
-from nulsexplorer.model.consensus import Consensus
-from nulsexplorer.model.transactions import Transaction
-from nulsexplorer.model.blocks import (Block, find_blocks, find_block,
-                                       get_last_block_height)
-from .utils import Pagination, PER_PAGE
+from aleph.web import app
 
 app.router.add_static('/static/',
-                      path=pkg_resources.resource_filename('pyaleph.web', 'static/'),
+                      path=pkg_resources.resource_filename('aleph.web', 'static/'),
                       name='static')
 
 @aiohttp_jinja2.template('index.html')
 async def index(request):
-    """Index of the block explorer.
+    """Index of aleph.
     """
-    last_blocks = [block async for block
-                   in Block.find({}, limit=10, sort=[('height', -1)])]
 
-    return {'last_blocks': last_blocks,
-            'last_height': await get_last_block_height()}
+    return {}
+
 app.router.add_get('/', index)
