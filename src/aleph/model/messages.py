51c94b3f107d287d75dc2106227d630a0b7e66ae
moshe malawach <moshe.malawach@protonmail.com>
2019-03-18 14:45:38 +0100

    add instant-computed aggregate

--- src/aleph/model/messages.py
+++ src/aleph/model/messages.py
@@ -22,3 +22,50 @@ class Message(BaseClass):
                Index("confirmations.height", pymongo.ASCENDING),
                Index("confirmations.height", pymongo.DESCENDING),
                Index("confirmed")]
+
+
+async def get_computed_address_aggregates(address_list=None, key_list=None):
+    aggregate = [
+        {'$match': {
+            'type': 'AGGREGATE'
+        }},
+        {'$group': {
+            '_id': {
+                'address': '$content.address',
+                'key': '$content.key'
+                },
+            'content': {
+                '$mergeObjects': '$content.content'
+            }
+        }},
+        {'$group': {
+            '_id': '$_id.address',
+            'items': {
+                '$push': {
+                    'k': '$_id.key',
+                    'v': '$content'
+                }
+            }
+        }},
+        {'$addFields': {
+            'address': '$_id',
+            'contents': {
+                '$arrayToObject': '$items'
+            }
+        }},
+        {'$project': {
+            '_id': 0,
+            'address': 1,
+            'contents': 1
+        }}
+    ]
+    if address_list is not None:
+        aggregate[0]['$match']['content.address'] = {'$in': address_list}
+
+    if key_list is not None:
+        aggregate[0]['$match']['content.key'] = {'$in': key_list}
+
+    results = Message.collection.aggregate(aggregate)
+
+    return {result['address']: result['contents']
+            async for result in results}
