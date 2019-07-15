78816bb2b0f6d825d5e03f14c29155848fc48205
moshe malawach <moshe.malawach@protonmail.com>
2019-07-15 10:29:25 +0200

    changed job type to manual without aiocron

--- src/aleph/jobs.py
+++ src/aleph/jobs.py
@@ -1,7 +1,5 @@
 from logging import getLogger
-import aiocron
 import asyncio
-from datetime import date, time, timezone, datetime, timedelta
 from aleph.chains.common import incoming
 from aleph.model.pending import PendingMessage
 from pymongo import DeleteOne
@@ -10,33 +8,62 @@ LOGGER = getLogger("JOBS")
 
 RETRY_LOCK = asyncio.Lock()
 
-@aiocron.crontab('*/30 * * * *', start=False)
+
+async def handle_pending(pending, actions_list):
+    result = await incoming(
+        pending['message'],
+        chain_name=pending['source'].get('chain_name'),
+        tx_hash=pending['source'].get('tx_hash'),
+        height=pending['source'].get('height'),
+        check_message=pending['source'].get('check_message', True),
+        retrying=True)
+
+    if result is True:  # message handled (valid or not, we don't care)
+        # Is it valid to add to a list passed this way? to be checked.
+        actions_list.append(DeleteOne({'_id': pending['_id']}))
+
+
+async def join_pending_message_tasks(tasks, actions_list):
+    try:
+        await asyncio.gather(*tasks)
+    except Exception:
+        LOGGER.exception("error in incoming task")
+    tasks.clear()
+
+    if len(actions_list):
+        await PendingMessage.collection.bulk_write(actions_list)
+        actions_list.clear()
+
+
 async def retry_job():
-    """ Each 30 minutes, try to handle message that were added to the
+    """ Each few minutes, try to handle message that were added to the
     pending queue (Unavailable messages)."""
 
-    col = PendingMessage.collection
-    if RETRY_LOCK.locked():
-        # Don't do the work twice.
-        return
+    actions = []
+    tasks = []
+    i = 0
+    async for pending in PendingMessage.collection.find():
+        i += 1
+        tasks.append(handle_pending(pending, actions))
 
-    async with RETRY_LOCK:
-        actions = []
-        async for pending in col.find():
-            result = await incoming(
-                pending['message'],
-                chain_name=pending['source'].get('chain_name'),
-                tx_hash=pending['source'].get('tx_hash'),
-                height=pending['source'].get('height'),
-                check_message=pending['source'].get('check_message', True),
-                retrying=True)
+        if (i > 200):
+            await join_pending_message_tasks(tasks, actions)
+            i = 0
 
-            if result is True:  # message handled (valid or not, we don't care)
-                actions.append(DeleteOne({'_id': pending['_id']}))
+    await join_pending_message_tasks(tasks, actions)
 
-        if len(actions):
-            await col.bulk_write(actions)
+
+async def retry_task():
+    while True:
+        try:
+            await retry_job()
+        except Exception:
+            LOGGER.exception("Error in pending messages retry job")
+
+        await asyncio.sleep(60)
 
 
 def start_jobs():
-    retry_job.start()
+    LOGGER.info("starting jobs")
+    loop = asyncio.get_event_loop()
+    loop.create_task(retry_task())
