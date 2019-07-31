5ac1153aa0aabd1327bd8db5a4becad5e1191b4b
moshe malawach <moshe.malawach@protonmail.com>
2019-07-31 12:08:53 +0200

    support inline messages

--- src/aleph/network.py
+++ src/aleph/network.py
@@ -3,13 +3,17 @@ import base64
 import base58
 import json
 import asyncio
+import hashlib
 from aleph.storage import get_base_url
 from aleph.chains.register import VERIFIER_REGISTER
 import logging
 LOGGER = logging.getLogger("NETWORK")
 
+MAX_INLINE_SIZE = 100000 # 100kb max inline content size.
+
 INCOMING_MESSAGE_AUTHORIZED_FIELDS = [
     'item_hash',
+    'item_content',
     'chain',
     'channel',
     'sender',
@@ -125,10 +129,35 @@ async def check_message(message, from_chain=False, from_network=False,
                         trusted=False):
     """ This function should check the incoming message and verify any
     extraneous or dangerous information for the rest of the process.
+    It also checks the data hash if it's not done by an external provider (ipfs)
+    and the data length.
     Example of dangerous data: fake confirmations, fake tx_hash, bad times...
+
+    If a item_content is there, set the item_type to inline, else to ipfs (default).
 
     TODO: Implement it fully! Dangerous!
     """
+    if message.get('item_content', None) is not None:
+        if len('item_content') > MAX_INLINE_SIZE:
+            LOGGER.warn('Message too long')
+            return None
+        
+        if message.get('hash_type', 'sha256') == 'sha256':  # leave the door open.
+            h = hashlib.sha256()
+            m.update(message['item_content'].encode('utf-8'))
+            
+            if message['item_hash'] != m.hexdigest():
+                LOGGER.warn('Bad hash')
+                return None
+        else:
+            LOGGER.warn('Unknown hash type %s' % message['hash_type'])
+            return None
+        
+        message['item_type'] = 'inline'
+        
+    else:
+        message['item_type'] = 'ipfs'
+    
     if trusted:
         # only in the case of a message programmatically built here
         # from legacy native chain signing for example (signing offloaded)
