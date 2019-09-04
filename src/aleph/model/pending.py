0406cdffd2275d7190f135316da02e8c78d1526b
moshe malawach <moshe.malawach@protonmail.com>
2019-09-04 15:55:35 +0200

    new pending tx model

--- src/aleph/model/pending.py
+++ src/aleph/model/pending.py
@@ -6,7 +6,7 @@ LOGGER = logging.getLogger('model.messages')
 
 class PendingMessage(BaseClass):
     """ Those messages have been received but their
-    content can't be retrieved. """
+    content will be retrieved later. """
     COLLECTION = "pending_messages"
 
     INDEXES = [IndexModel([("message.item_hash", ASCENDING)]),  # Content IPFS hash
@@ -16,3 +16,14 @@ class PendingMessage(BaseClass):
                IndexModel([("message.chain", ASCENDING)]),
                IndexModel([("source.chain_name", ASCENDING)]),
                IndexModel([("source.height", ASCENDING)]),]
+
+class PendingTX(BaseClass):
+    """ Those tx have been received onchain, but their
+    content will be processed later. """
+    COLLECTION = "pending_txs"
+
+    INDEXES = [IndexModel([("context.publisher", ASCENDING)]),
+               IndexModel([("context.time", DESCENDING)]),
+               IndexModel([("context.chain_name", ASCENDING)]),
+               IndexModel([("context.height", ASCENDING)]),
+               IndexModel([("context.tx_hash", ASCENDING)]),]
