4d444d37c4284b73ca3b20c780a840a61a1c12b8
moshe malawach <moshe.malawach@protonmail.com>
2019-09-05 18:47:17 +0200

    remove indexes

--- src/aleph/model/pending.py
+++ src/aleph/model/pending.py
@@ -9,21 +9,12 @@ class PendingMessage(BaseClass):
     content will be retrieved later. """
     COLLECTION = "pending_messages"
 
-    INDEXES = [IndexModel([("message.item_hash", ASCENDING)]),  # Content IPFS hash
-               IndexModel([("message.sender", ASCENDING)]),
-               IndexModel([("message.time", DESCENDING)]),
-               IndexModel([("message.time", ASCENDING)]),
-               IndexModel([("message.chain", ASCENDING)]),
-               IndexModel([("source.chain_name", ASCENDING)]),
-               IndexModel([("source.height", ASCENDING)]),]
+    INDEXES = [IndexModel([("message.time", DESCENDING)]),
+               IndexModel([("message.time", ASCENDING)])]
 
 class PendingTX(BaseClass):
     """ Those tx have been received onchain, but their
     content will be processed later. """
     COLLECTION = "pending_txs"
 
-    INDEXES = [IndexModel([("context.publisher", ASCENDING)]),
-               IndexModel([("context.time", DESCENDING)]),
-               IndexModel([("context.chain_name", ASCENDING)]),
-               IndexModel([("context.height", ASCENDING)]),
-               IndexModel([("context.tx_hash", ASCENDING)]),]
+    INDEXES = [IndexModel([("context.time", DESCENDING)])]
