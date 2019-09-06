f448900aac087d024edae3d8ee851c92793fc0f8
moshe malawach <moshe.malawach@protonmail.com>
2019-09-06 14:30:44 +0200

    woops (sc error)

--- src/aleph/chains/assets/ethereum_sc.sol
+++ src/aleph/chains/assets/ethereum_sc.sol
@@ -2,11 +2,10 @@ pragma solidity ^0.5.11;
 
 contract AlephSync{
 
-    event Emit(uint256 timestamp, address addr, string message); 
-    //emitter.doEmit("blah", {from: "[primary acc]", value: web3.toWei(100, "ether")});
+    event SyncEvent(uint256 timestamp, address addr, string message); 
     
     function doEmit(string memory message) public {
-        emit Emit(block.timestamp, msg.sender, message);
+        emit SyncEvent(block.timestamp, msg.sender, message);
     }
 
 }
