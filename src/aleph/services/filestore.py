9172a41eb1d90658aa3fde2ccdf70bd2ce42a9dd
Moshe Malawach <moshe.malawach@protonmail.com>
2019-09-26 00:30:40 +0200

    rollback rocksdb params

--- src/aleph/services/filestore.py
+++ src/aleph/services/filestore.py
@@ -7,21 +7,21 @@ hashes_db = None
 
 def init_store(config):
     global hashes_db
-    # opts = rocksdb.Options()
-    # opts.create_if_missing = True
-    # opts.max_open_files = 300000
-    # opts.write_buffer_size = 67108864
-    # opts.max_write_buffer_number = 3
-    # opts.target_file_size_base = 67108864
+    opts = rocksdb.Options()
+    opts.create_if_missing = True
+    opts.max_open_files = 300000
+    opts.write_buffer_size = 67108864
+    opts.max_write_buffer_number = 3
+    opts.target_file_size_base = 67108864
 
-    # opts.table_factory = rocksdb.BlockBasedTableFactory(
-    #     filter_policy=rocksdb.BloomFilterPolicy(10),
-    #     block_cache=rocksdb.LRUCache(2 * (1024 ** 3)),
-    #     block_cache_compressed=rocksdb.LRUCache(500 * (1024 ** 2)))
+    opts.table_factory = rocksdb.BlockBasedTableFactory(
+        filter_policy=rocksdb.BloomFilterPolicy(10),
+        block_cache=rocksdb.LRUCache(2 * (1024 ** 3)),
+        block_cache_compressed=rocksdb.LRUCache(500 * (1024 ** 2)))
 
-    # hashes_db = rocksdb.DB(os.path.join(config.storage.folder.value, HASHES_STORAGE), opts)
-    hashes_db = rocksdb.DB(os.path.join(config.storage.folder.value, HASHES_STORAGE),
-                           rocksdb.Options(create_if_missing=True))
+    hashes_db = rocksdb.DB(os.path.join(config.storage.folder.value, HASHES_STORAGE), opts)
+    # hashes_db = rocksdb.DB(os.path.join(config.storage.folder.value, HASHES_STORAGE),
+    #                        rocksdb.Options(create_if_missing=True))
     
 async def get_value(key):
     # if not isinstance(key, bytes):
