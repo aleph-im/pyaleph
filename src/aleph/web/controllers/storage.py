c9f1bd7f7571882a0c4515a8667d7ccc87e7a7ca
Moshe Malawach <moshe.malawach@protonmail.com>
2019-10-11 17:08:04 +0200

    api get hash content endpoint

--- src/aleph/web/controllers/storage.py
+++ src/aleph/web/controllers/storage.py
@@ -1,7 +1,8 @@
-from aleph.storage import add_json
+from aleph.storage import add_json, get_hash_content
 from aleph.web import app
 from aleph.services.ipfs.pubsub import pub
 from aiohttp import web
+import base64
 
 
 async def add_json_controller(request):
@@ -17,3 +18,29 @@ async def add_json_controller(request):
 
 app.router.add_post('/api/v0/storage/add_json', add_json_controller)
 app.router.add_post('/api/v0/ipfs/add_json', add_json_controller)
+
+async def get_hash(request):
+    result = {'status': 'error',
+              'reason': 'unknown'}
+    item_hash = request.match_info.get('hash', None)
+    
+    if hash is not None:
+        value = await get_hash_content(item_hash, use_network=False)
+        if value is not None and value != -1:
+            result = {'status': 'success',
+                      'hash': item_hash,
+                      'content': base64.encodebytes(value).decode('utf-8')}
+        else:
+            result = {'status': 'success',
+                      'hash': item_hash,
+                      'content': None}
+    else:
+        result = {'status': 'error',
+                'reason': 'no hash provided'}
+        
+    response = web.json_response(result)
+    response.enable_compression()
+    return response
+
+    
+app.router.add_get('/api/v0/storage/{hash}', get_hash)
