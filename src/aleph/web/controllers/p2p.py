f094947c11113b9f806664260e66942691bec571
Moshe Malawach <moshe.malawach@protonmail.com>
2019-10-16 17:22:25 +0200

    handle exceptions

--- src/aleph/web/controllers/p2p.py
+++ src/aleph/web/controllers/p2p.py
@@ -4,19 +4,31 @@ from aleph.web import app
 from aleph.services.ipfs.pubsub import pub as pub_ipfs
 from aleph.services.p2p import pub as pub_p2p
 from aiohttp import web
+import logging
+
+LOGGER = logging.getLogger('web.controllers.p2p')
 
 async def pub_json(request):
     """ Forward the message to P2P host and IPFS server as a pubsub message
     """
     data = await request.json()
-
-    if app['config'].ipfs.enabled.value:
-        await pub_ipfs(data.get('topic'), data.get('data'))
-        
-    await pub_p2p(data.get('topic'), data.get('data'))
+    status = "success"
+    try:
+        if app['config'].ipfs.enabled.value:
+            await pub_ipfs(data.get('topic'), data.get('data'))
+    except Exception:
+        LOGGER.exception("Can't publish on ipfs")
+        status = "warning"
+    
+    
+    try:
+        await pub_p2p(data.get('topic'), data.get('data'))
+    except Exception:
+        LOGGER.exception("Can't publish on p2p")
+        status = "warning"
 
     output = {
-        'status': 'success'
+        'status': status
     }
     return web.json_response(output)
 
