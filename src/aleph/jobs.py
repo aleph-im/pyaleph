f30f85d805f12ec5656becdae8c1da545eb0d779
Moshe Malawach <moshe.malawach@protonmail.com>
2019-10-21 11:38:22 +0200

    write messages before pending, so no data is lost

--- src/aleph/jobs.py
+++ src/aleph/jobs.py
@@ -46,13 +46,13 @@ async def join_pending_message_tasks(tasks, actions_list=None, messages_actions_
         LOGGER.exception("error in incoming task")
     tasks.clear()
 
-    if actions_list is not None and len(actions_list):
-        await PendingMessage.collection.bulk_write(actions_list)
-        actions_list.clear()
-
     if actions_list is not None and len(messages_actions_list):
         await Message.collection.bulk_write(messages_actions_list)
         messages_actions_list.clear()
+
+    if actions_list is not None and len(actions_list):
+        await PendingMessage.collection.bulk_write(actions_list)
+        actions_list.clear()
 
 
 async def retry_messages_job():
