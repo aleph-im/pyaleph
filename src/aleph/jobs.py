9fef4f7435dc699de4f87e5b2fd27bbdfcb2714d
Moshe Malawach <moshe.malawach@protonmail.com>
2019-10-22 11:03:54 +0200

    leave room for clean

--- src/aleph/jobs.py
+++ src/aleph/jobs.py
@@ -72,7 +72,7 @@ async def retry_messages_job():
     #     find_params = {'message.item_type': 'inline'}
         
     while await PendingMessage.collection.count_documents(find_params):
-        async for pending in PendingMessage.collection.find(find_params).sort([('message.time', 1)]).batch_size(1024):
+        async for pending in PendingMessage.collection.find(find_params).sort([('message.time', 1)]).batch_size(512).limit(1000000):
             if pending['message']['item_type'] == 'ipfs':
                 i += 15
                 j += 100
