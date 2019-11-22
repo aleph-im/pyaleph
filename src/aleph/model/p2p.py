93d25d4a31bb23e807436c64e64ab486b49f0fa0
Moshe Malawach <moshe.malawach@protonmail.com>
2019-11-22 13:16:07 +0100

    disregard old peers

--- src/aleph/model/p2p.py
+++ src/aleph/model/p2p.py
@@ -3,7 +3,7 @@
 
 from aleph.model.base import BaseClass
 from pymongo import ASCENDING, DESCENDING, IndexModel
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Peer(BaseClass):
     """Holds information about the chains state."""
@@ -18,7 +18,10 @@ async def get_peers(peer_type=None):
     TODO: handle the last seen, channel preferences, and better way of avoiding "bad contacts".
     NOTE: Currently used in jobs.
     """
-    async for peer in Peer.collection.find({'type': peer_type}).sort([('last_seen', -1)]):
+    async for peer in Peer.collection.find({
+        'type': peer_type,
+        'last_seen': {'$gt': datetime.now() - timedelta(hours=2)}
+        }).sort([('last_seen', -1)]):
         yield peer['address']
 
 async def add_peer(address, peer_type):
