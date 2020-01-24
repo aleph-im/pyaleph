2d35019dcb4b09f0ea60cb95e5cb446f686e1e88
Moshe Malawach <moshe.malawach@protonmail.com>
2020-01-24 10:51:14 +0100

    WIP containerized python VM

--- src/aleph/vms/dockerized/python.py
+++ src/aleph/vms/dockerized/python.py
@@ -6,7 +6,7 @@ import epicbox
 from aleph.vms.register import register_vm_engine
 from .base import DockerizedBaseVM
 
-class RestrictedPythonVM(DockerizedBaseVM):
+class DockerizedPythonVM(DockerizedBaseVM):
     
     __version__ = 0.1
     
@@ -28,32 +28,32 @@ class RestrictedPythonVM(DockerizedBaseVM):
             'args': args,
             'kwargs': kwargs
         }
-    
-    
-        raise NotImplementedError      
+        
+        output = cls._run('python', 'python3 executor.py',
+                          stdin=json.dumps(payload).encode('utf-8'))
+        
+        if output['status'] != 0:
+            return {'result': None, 'error': output['stderr']}
+        
+        try:
+            out_payload = json.loads(output['stdout'].decode('utf-8'))
+            return out_payload
+        except Exception as e:
+            return {'result': None, 'error': repr(e)}
         
         
     @classmethod
-    def call(cls, code, message, *args, **kwargs):
+    def call(cls, code, state, message, *args, **kwargs):
         """ Call a fonction on the VM.
         """
         payload = {
             'code': code,
             'action': 'call',
-            'function': 'transfer',
-            'message': {
-                'sender': 'NULSd6HgcNwprmEYbQ7pqLznGVU3EhW7Syv7W'
-            },
-            'state': {
-                "owner": "NULSd6HgcNwprmEYbQ7pqLznGVU3EhW7Syv7W",
-                "name": "Test",
-                "symbol": "TST",
-                "decimals": 18,
-                "total_supply": 24000000000000000000000000,
-                "balances": {"NULSd6HgcNwprmEYbQ7pqLznGVU3EhW7Syv7W": 24000000000000000000000000},
-                "allowed": {}
-            },        
-            'args': ['blah', 1000*(10**18)]
+            'function': message['content']['function'],
+            'message': message,
+            'state': state,        
+            'args': message['content'].get('args', []),
+            'kwargs': message['content'].get('kwargs', {})
         }
         
         output = cls._run('python', 'python3 executor.py',
@@ -64,12 +64,8 @@ class RestrictedPythonVM(DockerizedBaseVM):
         
         try:
             out_payload = json.loads(output['stdout'].decode('utf-8'))
-        assert out_payload['result'] is True
-        assert out_payload['state']['owner'] == 'NULSd6HgcNwprmEYbQ7pqLznGVU3EhW7Syv7W'
-        assert out_payload['state']['total_supply'] == 24000000 * (10**18)
-        assert out_payload['state']['balances']['NULSd6HgcNwprmEYbQ7pqLznGVU3EhW7Syv7W'] == (24000000 - 1000) * (10**18)
-        assert out_payload['state']['balances']['blah'] == 1000 * (10**18)
-        assert len(out_payload['state']['balances']) == 2
-        assert not output.get('stderr')    
+            return out_payload
+        except Exception as e:
+            return {'result': None, 'error': repr(e)}
 
-register_vm_engine('python_container', RestrictedPythonVM)
+register_vm_engine('python_container', DockerizedPythonVM)
