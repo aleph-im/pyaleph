71c758b2869e060af9c6aa8dad4f8ed0957f0b06
Moshe Malawach <moshe.malawach@protonmail.com>
2020-07-11 22:44:08 +0200

    use text ident me

--- src/aleph/services/utils.py
+++ src/aleph/services/utils.py
@@ -5,8 +5,8 @@ async def get_IP():
     ip = None
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get('https://v4.ident.me/.json') as resp:
-                ip = (await resp.json())['address']
+            async with session.get('https://v4.ident.me/') as resp:
+                ip = (await resp.text())
     except:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
