d4cf2b962e8ed3ea2370d4e5b3ecaa3345b87e97
Hugo Herter <git@hugoherter.com>
2020-12-01 14:17:07 +0100

    Problem: Operator could not get metrics
    Solution: Implement a Prometheus entrypoint on /metrics
    and expose aleph node metrics on it.

---
+++ src/aleph/web/controllers/metrics.py
@@ -0,0 +1,64 @@
+import json
+import platform
+from dataclasses import dataclass, asdict
+from typing import Dict
+
+import aleph.model
+from aleph import __version__
+
+
+def format_dict_for_prometheus(values: Dict) -> str:
+    """Format a dict to a Prometheus tags string"""
+    values = (f"{key}={json.dumps(value)}"
+              for key, value in values.items())
+    return '{' + ','.join(values) + '}'
+
+
+def format_dataclass_for_prometheus(instance) -> str:
+    """Turn a dataclass into Prometheus text format"""
+
+    result = []
+    for key, value in asdict(instance).items():
+        if isinstance(value, dict):
+            # Use a constant value of 1 for version, as Prometheus does
+            result.append(f"{key}{format_dict_for_prometheus(value)} 1")
+        else:
+            result.append(f"{key} {json.dumps(value)}")
+    return '\n'.join(result)
+
+
+@dataclass
+class BuildInfo:
+    """Dataclass used to export aleph node build info."""
+    python_version: str
+    version: str
+    # branch: str
+    # revision: str
+
+
+@dataclass
+class Metrics:
+    """Dataclass used to expose aleph node metrics.
+    """
+    pyaleph_build_info: BuildInfo
+    pyaleph_status_sync_messages_total: int
+    pyaleph_status_sync_pending_messages_total: int
+
+
+pyaleph_build_info = BuildInfo(
+    python_version=platform.python_version(),
+    version=__version__,
+)
+
+
+async def get_metrics() -> Metrics:
+
+    return Metrics(
+        pyaleph_build_info=pyaleph_build_info,
+
+        pyaleph_status_sync_messages_total=
+        await aleph.model.db.messages.count_documents({}),
+
+        pyaleph_status_sync_pending_messages_total=
+        await aleph.model.db.pending_messages.count_documents({}),
+    )
