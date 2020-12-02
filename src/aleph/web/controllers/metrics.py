730c33ed02e51b68b9d3b881bf0bff1db2ae0c91
Hugo Herter <git@hugoherter.com>
2020-12-02 17:24:12 +0100

    Feature: Add metric on ETH last committed height

--- src/aleph/web/controllers/metrics.py
+++ src/aleph/web/controllers/metrics.py
@@ -43,6 +43,7 @@ class Metrics:
     pyaleph_build_info: BuildInfo
     pyaleph_status_sync_messages_total: int
     pyaleph_status_sync_pending_messages_total: int
+    pyaleph_status_chain_eth_last_committed_height: int
 
 
 pyaleph_build_info = BuildInfo(
@@ -61,4 +62,9 @@ async def get_metrics() -> Metrics:
 
         pyaleph_status_sync_pending_messages_total=
         await aleph.model.db.pending_messages.count_documents({}),
+
+        pyaleph_status_chain_eth_last_committed_height=
+        (await aleph.model.db.chains.find_one({'name': 'ETH'},
+                                              projection={'last_commited_height': 1})
+         or {}).get('last_commited_height'),
     )
