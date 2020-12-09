89dc9896b4a80db3ac1491b261ccf3a71a928e3d
Hugo Herter <git@hugoherter.com>
2020-12-09 15:33:15 +0100

    Fix: Log exception on message retrieval

--- src/aleph/services/p2p/http.py
+++ src/aleph/services/p2p/http.py
@@ -27,7 +27,7 @@ async def api_get_request(base_uri, method, timeout=1):
             else:
                 result = await resp.json()
     except:
-        # LOGGER.exception("Error in retrieval")
+        LOGGER.exception("Error in retrieval")
         result = None
     return result
 
