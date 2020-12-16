95413a593cbec16dccb293f15e9ed3114ca5ccea
Hugo Herter <git@hugoherter.com>
2020-12-16 15:07:19 +0100

    Feat: Use git tags for version numbers
    Command "git describe --tags" gives an output in the form
    "v0.0.1" or "v0.0.1-abcdefgh", where "v0.0.1" is the name of the last tag and "abcdefgh" is the start of the last commit id.

--- src/aleph/__init__.py
+++ src/aleph/__init__.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
+import subprocess
+
 from pkg_resources import get_distribution, DistributionNotFound
+
+
+def get_git_version():
+    output = subprocess.check_output(('git', 'describe', '--tags'))
+    return output.decode().strip()
+
 
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
-    __version__ = 'unknown'
+    __version__ = get_git_version()
 finally:
     del get_distribution, DistributionNotFound
