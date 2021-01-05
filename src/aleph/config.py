f411390ba2e43df4564563f195baed3acbd5d30e
Moshe Malawach <moshe.malawach@protonmail.com>
2021-01-05 21:54:23 +0100

    default listen on 0.0.0.0

--- src/aleph/config.py
+++ src/aleph/config.py
@@ -3,7 +3,7 @@ def get_defaults():
     return {
         'aleph': {
             'queue_topic': 'ALEPH-QUEUE',
-            'host': '127.0.0.1',
+            'host': '0.0.0.0',
             'port': 8000,
             'reference_node_url': None,
         },
