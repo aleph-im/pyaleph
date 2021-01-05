e47c46dce96142a91e9e6c7cddaafe8e5e571e40
Jonathan Schemoul <jonathan.schemoul@gmail.com>
2021-01-05 15:06:59 +0100

    tentative nacl support

---
+++ src/aleph/chains/solana.py
@@ -0,0 +1,50 @@
+import json
+import base58
+from aleph.chains.common import get_verification_buffer
+from aleph.chains.register import register_verifier
+
+from nacl.signing import VerifyKey
+
+import logging
+LOGGER = logging.getLogger('chains.solana')
+CHAIN_NAME = 'SOL'
+
+
+async def verify_signature(message):
+    """ Verifies a signature of a message, return True if verified, false if not
+    """
+
+    try:
+        signature = json.loads(message['signature'])
+        sigdata = base58.b58decode(signature['signature'])
+        public_key = base58.b58decode(signature['publicKey'])
+    except Exception:
+        LOGGER.exception("Solana signature deserialization error")
+        return False
+
+    try:
+        if signature.get('version', 1) != 1:
+            LOGGER.warning('Unsupported signature version %d' % signature.get('version'))
+    except Exception:
+        LOGGER.exception("Solana signature version error")
+        return False
+
+    if message['sender'] != signature['publicKey']:
+        LOGGER.exception("Solana signature source error")
+        return False
+
+    try:
+        verify_key = VerifyKey(public_key)
+        verification_buffer = await get_verification_buffer(message)
+        verif = verify_key.verify(verification_buffer,
+                                  signature=sigdata)
+        result = (verif == verification_buffer)
+        # verif = (await get_verification_buffer(message)).decode('utf-8')
+        # result = keypair.verify(verif, signature['data'])
+    except Exception:
+        LOGGER.exception("Substrate Signature verification error")
+        result = False
+
+    return result
+
+register_verifier(CHAIN_NAME, verify_signature)
