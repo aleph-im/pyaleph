742d51f7a162e076cab7e64e58e0f250969b5fed
Hugo Herter <git@hugoherter.com>
2021-09-29 11:52:41 +0200

    Chore: Use distinct script for 'alpha' releases

---
+++ deployment/docker-build/publish-alpha.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Use this script to publish the current Docker image of PyAleph on Docker Hub
+
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "$SCRIPT_DIR/../.."
+
+# Use Podman if installed, else use Docker
+if hash podman 2> /dev/null
+then
+  DOCKER_COMMAND=podman
+else
+  DOCKER_COMMAND=docker
+fi
+
+VERSION=$(git describe --tags)-alpha
+
+$DOCKER_COMMAND tag alephim/pyaleph-node alephim/pyaleph-node:$VERSION
+$DOCKER_COMMAND push alephim/pyaleph-node:$VERSION docker.io/alephim/pyaleph-node:$VERSION
+echo docker.io/alephim/pyaleph-node:$VERSION
