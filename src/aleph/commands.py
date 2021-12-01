427f8a92b7752a3b9594301786f695bdc7f20540
Hugo Herter <git@hugoherter.com>
2021-12-01 11:07:40 +0100

    Refactor: Move CLI args in submodule

--- src/aleph/commands.py
+++ src/aleph/commands.py
@@ -10,7 +10,6 @@ Besides console scripts, the header (i.e. until _logger...) of this file can
 also be used as template for Python modules.
 """
 
-import argparse
 import asyncio
 import logging
 import sys
@@ -20,9 +19,9 @@ from typing import List, Coroutine, Dict, Optional
 from configmanager import Config
 from setproctitle import setproctitle
 
-from aleph import __version__
 from aleph import model
 from aleph.chains import connector_tasks
+from aleph.cli.args import parse_args
 from aleph.config import get_defaults
 from aleph.jobs import start_jobs, prepare_loop, prepare_manager, DBManager
 from aleph.network import listener_tasks
@@ -38,82 +37,6 @@ __copyright__ = "Moshe Malawach"
 __license__ = "mit"
 
 LOGGER = logging.getLogger(__name__)
-
-
-def parse_args(args):
-    """Parse command line parameters
-
-    Args:
-      args ([str]): command line parameters as list of strings
-
-    Returns:
-      :obj:`argparse.Namespace`: command line parameters namespace
-    """
-    parser = argparse.ArgumentParser(prog="aleph", description="Aleph Network Node")
-    parser.add_argument(
-        "--version", action="version", version="pyaleph {ver}".format(ver=__version__)
-    )
-    parser.add_argument("-c", "--config", action="store", dest="config_file")
-    parser.add_argument(
-        "-p", "--port", action="store", type=int, dest="port", required=False
-    )
-    parser.add_argument(
-        "--bind", "-b", action="store", type=str, dest="host", required=False
-    )
-    parser.add_argument("--debug", action="store_true", dest="debug", default=False)
-    parser.add_argument(
-        "--no-commit", action="store_true", dest="no_commit", default=False
-    )
-    parser.add_argument("--no-jobs", action="store_true", dest="no_jobs", default=False)
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        dest="loglevel",
-        help="set loglevel to INFO",
-        action="store_const",
-        const=logging.INFO,
-        default=logging.WARNING,
-    )
-    parser.add_argument(
-        "-vv",
-        "--very-verbose",
-        dest="loglevel",
-        help="set loglevel to DEBUG",
-        action="store_const",
-        const=logging.DEBUG,
-    )
-    parser.add_argument(
-        "-g",
-        "--gen-key",
-        dest="generate_key",
-        help="Generate a node key and exit",
-        action="store_true",
-        default=False,
-    )
-    parser.add_argument(
-        "--print-key",
-        dest="print_key",
-        help="Print the generated key",
-        action="store_true",
-        default=False,
-    )
-    parser.add_argument(
-        "-k",
-        "--key",
-        dest="key_path",
-        help="Path to the node private key",
-        action="store",
-        type=str,
-        default="node-secret.key",
-    )
-    parser.add_argument(
-        "--disable-sentry",
-        dest="sentry_disabled",
-        help="Disable Sentry error tracking",
-        action="store_true",
-        default=False,
-    )
-    return parser.parse_args(args)
 
 
 def setup_logging(loglevel: int):
