0bb48b36b183f2e7d713ffef9669059fe8773be9
Hugo Herter <git@hugoherter.com>
2022-02-21 20:34:25 +0100

    Fix: On some servers, ip returned as chinese chars instead if IPv4
    The bug only happened when using aiohttp and was not reproduced using requests.

--- src/aleph/services/utils.py
+++ src/aleph/services/utils.py
@@ -1,14 +1,16 @@
 import socket
 
-import aiohttp
+# import aiohttp
+import requests
 
 
 async def get_IP():
     ip = None
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get("https://v4.ident.me/") as resp:
-                ip = await resp.text()
+        # async with aiohttp.ClientSession() as session:
+        #     async with session.get("https://v4.ident.me/") as resp:
+        #         ip = await resp.text()
+        ip = requests.get("https://v4.ident.me/").text
     except:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
