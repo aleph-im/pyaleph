c5aae540e9e37f388a959a1121742b0eb426449b
Hugo Herter <git@hugoherter.com>
2022-02-22 12:43:14 +0100

    Fix: Protect against wrong IPv4 format from 3rd party service.
     On some servers, get_IP() returned chinese chars instead if a proper IPv4. This protects against invalid IP formats.

---
+++ tests/services/test_utils.py
@@ -0,0 +1,33 @@
+import pytest
+
+from aleph.services.utils import (
+    is_valid_ip4,
+    get_ip4_from_service,
+    get_ip4_from_socket,
+    get_IP,
+)
+
+
+def test_is_valid_ip4():
+    assert is_valid_ip4("1.2.3.4")
+    assert is_valid_ip4("123.456.789.123")
+    assert not is_valid_ip4("")
+    assert not is_valid_ip4("Hello !")
+    assert not is_valid_ip4("a.b.c.d")
+
+
+@pytest.mark.asyncio
+async def test_get_ip4_from_service():
+    ip4 = await get_ip4_from_service()
+    assert is_valid_ip4(ip4)
+
+
+def test_get_ip4_from_socket():
+    ip4 = get_ip4_from_socket()
+    assert is_valid_ip4(ip4)
+
+
+@pytest.mark.asyncio
+async def test_get_IP():
+    ip4 = await get_IP()
+    assert is_valid_ip4(ip4)
