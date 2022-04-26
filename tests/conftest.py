694b10f41b762f0d8687e5248d1c1e2b79d5dc6c
Olivier Desenfans <desenfans.olivier@gmail.com>
2022-04-26 12:24:46 +0200

    [Tests] Database support in tests (#232)
    Added a fixture to initialize the database. This fixture initializes
    the collections used by a CCN in a separate database and resets
    it on every run.
    
    This feature will allow to write more extensive tests by preloading
    fixture data in the DB.
    
    Implemented a first test using the fixture for the pending TX job.
    On the CI side, we now spawn a service container to run MongoDB.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,30 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-    Dummy conftest.py for pyaleph.
+import pytest
+from aleph.model import init_db
+from aleph.config import get_defaults
+from configmanager import Config
+import pymongo
+import pytest_asyncio
 
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    https://pytest.org/latest/plugins.html
-"""
 
-# import pytest
+TEST_DB = "ccn_automated_tests"
+
+
+def drop_db(db_name: str, config: Config):
+    client = pymongo.MongoClient(config.mongodb.uri.value)
+    client.drop_database(db_name)
+
+
+@pytest_asyncio.fixture
+async def test_db():
+    """
+    Initializes and cleans a MongoDB database dedicated to automated tests.
+    """
+
+    config = Config(schema=get_defaults())
+    config.mongodb.database.value = TEST_DB
+
+    drop_db(TEST_DB, config)
+    init_db(config, ensure_indexes=True)
+
+    from aleph.model import db
+    yield db
