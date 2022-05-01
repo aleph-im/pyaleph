038695bc033501b071d614ec5fce0ca37b83acd6
Olivier Desenfans <desenfans.olivier@gmail.com>
2022-05-01 20:54:48 +0200

    add comment for time field

--- src/aleph/chains/tx_context.py
+++ src/aleph/chains/tx_context.py
@@ -6,5 +6,6 @@ class TxContext:
     chain_name: str
     tx_hash: str
     height: int
+    # Transaction timestamp, in Unix time (number of seconds since epoch).
     time: int
     publisher: str
