c58b152af44472adeac3f66f54d4f844e8722fa7
Olivier Desenfans <desenfans.olivier@gmail.com>
2022-10-06 22:51:16 +0200

    fix for review: add docstrings to utils

--- tests/api/utils/__init__.py
+++ tests/api/utils/__init__.py
@@ -4,10 +4,24 @@ from typing import Dict, Iterable, List, Callable
 def get_messages_by_predicate(
     messages: Iterable[Dict], predicate: Callable[[Dict], bool]
 ) -> List[Dict]:
+    """
+    Filters messages based on a user-provided predicate
+    (=a function/lambda operating on a single message).
+    """
+
     return [msg for msg in messages if predicate(msg)]
 
 
 def get_messages_by_keys(messages: Iterable[Dict], **keys) -> List[Dict]:
+    """
+    Filters messages based on user-provided keys.
+
+    Example:
+    >>> filtered_messages = get_messages_by_keys(
+    >>>     message_list, item_hash="some-hash", channel="MY-CHANNEL"
+    >>> )
+
+    """
     return get_messages_by_predicate(
         messages, lambda msg: all(msg[k] == v for k, v in keys.items())
     )
