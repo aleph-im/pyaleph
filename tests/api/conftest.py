6e2ea65fbcd412dde983a4f7141ca429b727775b
Olivier Desenfans <desenfans.olivier@gmail.com>
2022-10-07 00:43:49 +0200

    Internal: add unit tests for the aggregates endpoint (#340)
    Problem: the /api/v0/aggregates/{address}.json endpoint was not
    tested enough.
    
    Solution: add tests.

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,16 +1,32 @@
 import json
 from pathlib import Path
+from typing import Dict, List
+
 import pytest_asyncio
 from aleph.model.messages import Message
 
 
-@pytest_asyncio.fixture
-async def fixture_messages(test_db):
+async def _load_fixtures(filename: str):
     fixtures_dir = Path(__file__).parent / "fixtures"
-    fixtures_file = fixtures_dir / "fixture_messages.json"
+    fixtures_file = fixtures_dir / filename
 
     with fixtures_file.open() as f:
         messages = json.load(f)
 
     await Message.collection.insert_many(messages)
     return messages
+
+
+@pytest_asyncio.fixture
+async def fixture_messages(test_db) -> List[Dict]:
+    return await _load_fixtures("fixture_messages.json")
+
+
+@pytest_asyncio.fixture
+async def fixture_aggregate_messages(test_db) -> List[Dict]:
+    return await _load_fixtures("fixture_aggregates.json")
+
+
+@pytest_asyncio.fixture
+async def fixture_post_messages(test_db) -> List[Dict]:
+    return await _load_fixtures("fixture_posts.json")
