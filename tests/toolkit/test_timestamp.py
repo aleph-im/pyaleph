f0580fb94ffe0d6eece69c747bbb6bf4db6e368f
Olivier Desenfans <desenfans.olivier@gmail.com>
2023-01-17 11:24:17 +0100

    Feature: exponential message retry times
    Problem: the current implementation of message retries may not leave
    enough time for the situation that cause the message processing failure
    to resolve itself, resulting in wrongly discarded messages.
    
    Solution: implement exponential retry times. With exponential retry
    times, messages are retried less times (10 times instead of 1000
    in the default configuration) and are guaranteed to remain ~15
    minutes in the processing queue.

---
+++ tests/toolkit/test_timestamp.py
@@ -0,0 +1,17 @@
+import pytz
+
+from aleph.toolkit.timestamp import timestamp_to_datetime
+
+
+def test_timestamp_to_datetime():
+    t1 = 1675206096.0  # 20230201T00:01:36+01:00
+    dt1 = timestamp_to_datetime(t1)
+
+    assert dt1.year == 2023
+    assert dt1.month == 1
+    assert dt1.day == 31
+    assert dt1.hour == 23
+    assert dt1.minute == 1
+    assert dt1.second == 36
+
+    assert dt1.tzinfo == pytz.utc
