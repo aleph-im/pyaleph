79067e5ed41c73f6f6d61b2113c305ec32d29ba4
Olivier Desenfans <desenfans.olivier@gmail.com>
2023-03-02 11:31:02 +0100

    Fix: default data storage folder
    Problem: the default folder to store files is not the same as
    the one used in example Docker Compose files.
    
    Solution: use /var/lib/pyaleph as default storage folder.

--- src/aleph/config.py
+++ src/aleph/config.py
@@ -47,7 +47,7 @@ def get_defaults():
             ],
             "topics": ["ALIVE", "ALEPH-TEST"],
         },
-        "storage": {"folder": "./data/", "store_files": True, "engine": "filesystem"},
+        "storage": {"folder": "/var/lib/pyaleph", "store_files": True, "engine": "filesystem"},
         "nuls": {
             "chain_id": 8964,
             "enabled": False,
