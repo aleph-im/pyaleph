2ffeab5df844bdd4e2be28c5c25d711d9c3f529f
Olivier Desenfans <desenfans.olivier@gmail.com>
2023-03-06 18:26:04 +0100

    Fix: slow response times on addresses/stats.json
    Problem: the response time of the stats endpoint is too slow because
    we perform a group by query on the whole message table every time.
    
    Solution: go back to caching. We use a postgres materialized view
    to this purpose to make this cache available to other endpoints.

---
+++ deployment/migrations/versions/0010_8a5eaab15d40_address_stats_view.py
@@ -0,0 +1,31 @@
+"""address stats view
+
+Revision ID: 8a5eaab15d40
+Revises: 8edf69c47884
+Create Date: 2023-03-06 17:27:14.514803
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8a5eaab15d40'
+down_revision = '8edf69c47884'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute(
+        """
+        create materialized view address_stats_mat_view as 
+            select sender as address, type, count(*) as nb_messages
+                from messages
+                group by sender, type
+        """)
+    op.execute("create unique index ix_address_type on address_stats_mat_view(address, type)")
+
+
+def downgrade() -> None:
+    op.execute("drop materialized view address_stats_mat_view")
