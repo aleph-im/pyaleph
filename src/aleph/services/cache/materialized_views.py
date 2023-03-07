34636b00733802dc92e7ec94cdefb9bcb603ee11
Olivier Desenfans <desenfans.olivier@gmail.com>
2023-03-07 15:45:43 +0100

    Fix: slow response times on addresses/stats.json (#377)
    Problem: the response time of the stats endpoint is too slow because
    we perform a group by query on the whole message table every time.
    
    Solution: go back to caching. We use a postgres materialized view
    to this purpose to make this cache available to other endpoints.

---
+++ src/aleph/services/cache/materialized_views.py
@@ -0,0 +1,29 @@
+import logging
+
+from aleph.db.accessors.messages import refresh_address_stats_mat_view
+from aleph.types.db_session import DbSessionFactory
+import asyncio
+
+LOGGER = logging.getLogger(__name__)
+
+
+async def refresh_cache_materialized_views(session_factory: DbSessionFactory) -> None:
+    """
+    Refresh DB materialized views used as caches, periodically.
+
+    Materialized views are a simple solution to cache expensive DB queries, at the cost
+    of refreshing them manually once in a while. This background task does exactly that.
+    Note that materialized views used by the API should support concurrent refreshing
+    to reduce latency.
+    """
+
+    while True:
+        try:
+            with session_factory() as session:
+                refresh_address_stats_mat_view(session)
+                LOGGER.info("Refreshed address stats materialized view")
+
+        except Exception:
+            LOGGER.exception("Error refreshing cache materialized views")
+
+        await asyncio.sleep(10 * 60)
