bb3e11e14ade7bb4e16cd769f1d9b846a865122a
1yam <lyam.gomes@epitech.eu>
2023-08-08 11:22:34 +0200

    Fix: No balance test

--- tests/message_processing/test_process_instances.py
+++ tests/message_processing/test_process_instances.py
@@ -146,6 +146,103 @@ def fixture_instance_message(session_factory: DbSessionFactory) -> PendingMessag
 
     return pending_message
 
+@pytest.fixture
+def fixture_instance_message_no_balance(session_factory: DbSessionFactory) -> PendingMessageDb:
+    content = {
+        "address": "0x9319Ad3B7A8E0eE24f2E639c40D8eD124C5520Ba",
+        "allow_amend": False,
+        "variables": {
+            "VM_CUSTOM_VARIABLE": "SOMETHING",
+            "VM_CUSTOM_VARIABLE_2": "32",
+        },
+        "environment": {
+            "reproducible": True,
+            "internet": False,
+            "aleph_api": False,
+            "shared_cache": False,
+        },
+        "resources": {"vcpus": 1, "memory": 128, "seconds": 30},
+        "requirements": {"cpu": {"architecture": "x86_64"}},
+        "rootfs": {
+            "parent": {
+                "ref": "549ec451d9b099cad112d4aaa2c00ac40fb6729a92ff252ff22eef0b5c3cb613",
+                "use_latest": True,
+            },
+            "persistence": "host",
+            "name": "test-rootfs",
+            "size_mib": 20000,
+        },
+        "authorized_keys": [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGULT6A41Msmw2KEu0R9MvUjhuWNAsbdeZ0DOwYbt4Qt user@example",
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH0jqdc5dmt75QhTrWqeHDV9xN8vxbgFyOYs2fuQl7CI",
+        ],
+        "volumes": [
+            {
+                "comment": "Python libraries. Read-only since a 'ref' is specified.",
+                "mount": "/opt/venv",
+                "ref": "5f31b0706f59404fad3d0bff97ef89ddf24da4761608ea0646329362c662ba51",
+                "use_latest": False,
+            },
+            {
+                "comment": "Ephemeral storage, read-write but will not persist after the VM stops",
+                "mount": "/var/cache",
+                "ephemeral": True,
+                "size_mib": 5,
+            },
+            {
+                "comment": "Working data persisted on the VM supervisor, not available on other nodes",
+                "mount": "/var/lib/sqlite",
+                "name": "sqlite-data",
+                "persistence": "host",
+                "size_mib": 10,
+            },
+            {
+                "comment": "Working data persisted on the Aleph network. "
+                           "New VMs will try to use the latest version of this volume, "
+                           "with no guarantee against conflicts",
+                "mount": "/var/lib/statistics",
+                "name": "statistics",
+                "persistence": "store",
+                "size_mib": 10,
+            },
+            {
+                "comment": "Raw drive to use by a process, do not mount it",
+                "name": "raw-data",
+                "persistence": "host",
+                "size_mib": 10,
+            },
+        ],
+        "time": 1619017773.8950517,
+    }
+
+    pending_message = PendingMessageDb(
+        item_hash="734a1287a2b7b5be060312ff5b05ad1bcf838950492e3428f2ac6437a1acad26",
+        type=MessageType.instance,
+        chain=Chain.ETH,
+        sender="0x9319Ad3B7A8E0eE24f2E639c40D8eD124C5520Ba",
+        signature=None,
+        item_type=ItemType.inline,
+        item_content=json.dumps(content),
+        time=timestamp_to_datetime(1619017773.8950577),
+        channel=None,
+        reception_time=timestamp_to_datetime(1619017774),
+        fetched=True,
+        check_message=False,
+        retries=0,
+        next_attempt=dt.datetime(2023, 1, 1),
+    )
+    with session_factory() as session:
+        session.add(pending_message)
+        session.add(
+            MessageStatusDb(
+                item_hash=pending_message.item_hash,
+                status=MessageStatus.PENDING,
+                reception_time=pending_message.reception_time,
+            )
+        )
+        session.commit()
+
+    return pending_message
 
 @pytest.fixture
 def fixture_forget_instance_message(
@@ -399,19 +496,19 @@ async def test_forget_instance_message(
 async def test_process_instance_balance(
         session_factory: DbSessionFactory,
         message_processor: PendingMessageProcessor,
-        fixture_instance_message: PendingMessageDb,
+        fixture_instance_message_no_balance: PendingMessageDb,
 ):
     with session_factory() as session:
-        insert_volume_refs(session, fixture_instance_message)
+        insert_volume_refs(session, fixture_instance_message_no_balance)
         session.commit()
 
     pipeline = message_processor.make_pipeline()
     # Exhaust the iterator
     _ = [message async for message in pipeline]
 
-    assert fixture_instance_message.item_content
-    content_dict = json.loads(fixture_instance_message.item_content)
+    assert fixture_instance_message_no_balance.item_content
+    content_dict = json.loads(fixture_instance_message_no_balance.item_content)
 
     with session_factory() as session:
-        rejected_message = get_rejected_message(session=session, item_hash=fixture_instance_message.item_hash)
-        assert rejected_message.error_code == ErrorCode.BALANCE_INSUFFICIENT
+        rejected_message = get_rejected_message(session=session, item_hash=fixture_instance_message_no_balance.item_hash)
+        assert rejected_message is not None
