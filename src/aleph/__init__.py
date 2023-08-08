9f1738a342f014d37e5d24c60a6cfa92e5e3b29a
1yam <lyam.gomes@epitech.eu>
2023-08-08 11:55:40 +0200

    Fix: re add __version__

--- src/aleph/__init__.py
+++ src/aleph/__init__.py
@@ -14,6 +14,6 @@ try:
     dist_name = __name__
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
-    __version__ = "1.4"
+    __version__ = _get_git_version()
 finally:
     del get_distribution, DistributionNotFound
