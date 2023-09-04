b5f248d1ec06d78b11116e51e057d7922aa80133
1yam <lyam.gomes@epitech.eu>
2023-09-04 16:27:04 +0200

    Fix: Black

--- tests/api/test_balance.py
+++ tests/api/test_balance.py
@@ -10,12 +10,13 @@ MESSAGES_URI = "/api/v0/addresses/0x9319Ad3B7A8E0eE24f2E639c40D8eD124C5520Ba/bal
 
 
 @pytest.mark.asyncio
-async def test_get_balance(ccn_api_client,
-                           message_processor: PendingMessageProcessor,
-                           instance_message_with_volumes_in_db,
-                           fixture_instance_message,
-                           user_balance: AlephBalanceDb
-                           ):
+async def test_get_balance(
+    ccn_api_client,
+    message_processor: PendingMessageProcessor,
+    instance_message_with_volumes_in_db,
+    fixture_instance_message,
+    user_balance: AlephBalanceDb,
+):
     pipeline = message_processor.make_pipeline()
     # Exhaust the iterator
     _ = [message async for message in pipeline]
