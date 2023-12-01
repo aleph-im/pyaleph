2cfa2e849c6e7d31b35c1262102da2ccbaee8bf0
1yam <lyam.gomes@epitech.eu>
2023-12-01 12:10:16 +0100

    Internal: Unit Test for metric endpoint

---
+++ tests/api/test_new_metric.py
@@ -0,0 +1,110 @@
+from typing import Any, Dict, Sequence
+
+import pytest
+import pytest_asyncio
+
+from src.aleph.types.db_session import DbSessionFactory
+
+from .conftest import _load_fixtures
+
+
+def _generate_uri(node_type: str, node_id: str) -> str:
+    return f"/api/v0/{node_type}/{node_id}/metrics"
+
+
+@pytest_asyncio.fixture
+async def fixture_metrics_messages(
+    session_factory: DbSessionFactory,
+) -> Sequence[Dict[str, Any]]:
+    return await _load_fixtures(session_factory, "test-metric.json")
+
+
+@pytest.mark.asyncio
+async def test_node_core_metrics(fixture_metrics_messages, ccn_api_client):
+    uri = _generate_uri(
+        "core", "b8b9104da69c54e58531212234fa31f49ef4c668a39a0bf6793322407857b821"
+    )
+    response = await ccn_api_client.get(uri)
+    test_data = await response.json()
+
+    assert response.status == 200
+    assert (
+        test_data["metrics"]["item_hash"][0]
+        == "56c82c6d3b28b76456594b4b57154b6826a6d5fb97d355d0428e5ca7d08193b9"
+    )
+    assert (
+        test_data["metrics"]["item_hash"][1]
+        == "172bab8f624fff1be70a19fecd45ff51fa4f833a34074451c7d79ece19bf37f0"
+    )
+
+
+@pytest.mark.asyncio
+async def test_node_core_metrics_sort(fixture_metrics_messages, ccn_api_client):
+    uri = _generate_uri(
+        "core", "b8b9104da69c54e58531212234fa31f49ef4c668a39a0bf6793322407857b821"
+    )
+    response = await ccn_api_client.get(uri, params={"sort": "DESC"})
+    test_data = await response.json()
+
+    assert response.status == 200
+    assert (
+        test_data["metrics"]["item_hash"][1]
+        == "56c82c6d3b28b76456594b4b57154b6826a6d5fb97d355d0428e5ca7d08193b9"
+    )
+    assert (
+        test_data["metrics"]["item_hash"][0]
+        == "172bab8f624fff1be70a19fecd45ff51fa4f833a34074451c7d79ece19bf37f0"
+    )
+
+
+@pytest.mark.asyncio
+async def test_node_core_metrics_end_date(fixture_metrics_messages, ccn_api_client):
+    uri = _generate_uri(
+        "core", "b8b9104da69c54e58531212234fa31f49ef4c668a39a0bf6793322407857b821"
+    )
+    response = await ccn_api_client.get(uri, params={"end_date": 1701261023})
+    test_data = await response.json()
+
+    assert response.status == 200
+    assert (
+        test_data["metrics"]["item_hash"][0]
+        == "56c82c6d3b28b76456594b4b57154b6826a6d5fb97d355d0428e5ca7d08193b9"
+    )
+
+
+@pytest.mark.asyncio
+async def test_node_core_metrics_start_date(fixture_metrics_messages, ccn_api_client):
+    uri = _generate_uri(
+        "core", "b8b9104da69c54e58531212234fa31f49ef4c668a39a0bf6793322407857b821"
+    )
+    response = await ccn_api_client.get(uri, params={"start_date": 1701261023})
+    test_data = await response.json()
+
+    assert response.status == 200
+    assert (
+        test_data["metrics"]["item_hash"][0]
+        == "172bab8f624fff1be70a19fecd45ff51fa4f833a34074451c7d79ece19bf37f0"
+    )
+
+
+@pytest.mark.asyncio
+async def test_node_core_not_exist(fixture_metrics_messages, ccn_api_client):
+    uri = _generate_uri("core", "This_is_a_node_id")
+    response = await ccn_api_client.get(uri)
+
+    assert response.status == 404
+
+
+@pytest.mark.asyncio
+async def test_node_compute_metric(fixture_metrics_messages, ccn_api_client):
+    uri = _generate_uri(
+        "compute", "d491f38ec66fe23a9c9ad398a04fd4dcb44a115b948ef612db844caea85cd59a"
+    )
+    response = await ccn_api_client.get(uri)
+    test_data = await response.json()
+
+    assert response.status == 200
+    assert (
+        test_data["metrics"]["item_hash"][0]
+        == "56c82c6d3b28b76456594b4b57154b6826a6d5fb97d355d0428e5ca7d08193b9"
+    )
