7b2bfba9d1c2dd3baa93be80ff41f4c31244cfd6
1yam <lyam.gomes@epitech.eu>
2023-12-01 12:54:16 +0100

    Refactor: Unit Test & remove too big message content for unit

--- tests/api/test_new_metric.py
+++ tests/api/test_new_metric.py
@@ -41,7 +41,7 @@ async def test_node_core_metrics(fixture_metrics_messages, ccn_api_client):
 @pytest.mark.asyncio
 async def test_node_core_metrics_sort(fixture_metrics_messages, ccn_api_client):
     uri = _generate_uri(
-        "core", "b8b9104da69c54e58531212234fa31f49ef4c668a39a0bf6793322407857b821"
+        "core", "2e7cd67ff8f556b0b3fb8a2ef8ab0e8e1466cfa279dd7b9bfbc8aba92e0c5672"
     )
     response = await ccn_api_client.get(uri, params={"sort": "DESC"})
     test_data = await response.json()
@@ -108,3 +108,12 @@ async def test_node_compute_metric(fixture_metrics_messages, ccn_api_client):
         test_data["metrics"]["item_hash"][0]
         == "56c82c6d3b28b76456594b4b57154b6826a6d5fb97d355d0428e5ca7d08193b9"
     )
+
+@pytest.mark.asyncio
+async def test_node_compute_metric_not_exist(fixture_metrics_messages, ccn_api_client):
+    uri = _generate_uri(
+        "compute", "This_is_a_node_id"
+    )
+    response = await ccn_api_client.get(uri)
+
+    assert response.status == 404
