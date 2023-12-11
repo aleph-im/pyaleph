19df1e4169c8afe070059296c1b23ca1b40d4343
1yam <40899431+1yam@users.noreply.github.com>
2023-12-11 10:32:23 +0100

    Feature: New Metrics endpoint for CRN & CNN (#528)
    * Feature: New metric endpoint for CRN & CNN
    
    * Internal: Unit Test for metric endpoint
    
    * Fix: Remove unused import
    
    * Refactor: Unit Test & remove too big message content for unit
    
    * Fix: mypy error
    
    * Refactor: easier way to parse sql query
    
    * Fix typo
    
    ---------
    
    Co-authored-by: Mike Hukiewitz <70762838+MHHukiewitz@users.noreply.github.com>

---
+++ src/aleph/db/accessors/metrics.py
@@ -0,0 +1,124 @@
+from typing import Optional
+
+from sqlalchemy import select, text
+from sqlalchemy.orm.session import Session
+
+from aleph.types.db_session import DbSession
+
+
+def _parse_ccn_result(result):
+    keys = [
+        "item_hash",
+        "measured_at",
+        "base_latency",
+        "base_latency_ipv4",
+        "metrics_latency",
+        "aggregate_latency",
+        "file_download_latency",
+        "pending_messages",
+        "eth_height_remaining",
+    ]
+
+    # Transpose the result and create a dictionary
+    result_dict = {key: list(values) for key, values in zip(keys, zip(*result))}
+
+    return result_dict
+
+
+def _parse_crn_result(result):
+    keys = [
+        "item_hash",
+        "measured_at",
+        "base_latency",
+        "base_latency_ipv4",
+        "full_check_latency",
+        "diagnostic_vm_latency",
+    ]
+
+    # Transpose the result and create a dictionary
+    result_dict = {key: list(values) for key, values in zip(keys, zip(*result))}
+
+    return result_dict
+
+
+def _build_metric_filter(select_stmt, node_id, start_date, end_date, sort_order):
+    if node_id:
+        select_stmt = select_stmt.where(text("node_id = :node_id")).params(
+            node_id=node_id
+        )
+    if start_date:
+        select_stmt = select_stmt.where(text("measured_at >= :start_date")).params(
+            start_date=start_date
+        )
+    if end_date:
+        select_stmt = select_stmt.where(text("measured_at <= :end_date")).params(
+            end_date=end_date
+        )
+    if sort_order:
+        select_stmt = select_stmt.order_by(text(f"measured_at {sort_order}"))
+    return select_stmt
+
+
+def query_metric_ccn(
+    session: Session,
+    node_id: Optional[str] = None,
+    start_date: Optional[float] = None,
+    end_date: Optional[float] = None,
+    sort_order: Optional[str] = None,
+):
+    select_stmt = select(
+        [
+            text("item_hash"),
+            text("measured_at"),
+            text("base_latency"),
+            text("base_latency_ipv4"),
+            text("metrics_latency"),
+            text("aggregate_latency"),
+            text("file_download_latency"),
+            text("pending_messages"),
+            text("eth_height_remaining"),
+        ]
+    ).select_from(text("ccn_metric_view"))
+
+    select_stmt = _build_metric_filter(
+        select_stmt=select_stmt,
+        node_id=node_id,
+        start_date=start_date,
+        end_date=end_date,
+        sort_order=sort_order,
+    )
+
+    result = session.execute(select_stmt).fetchall()
+
+    return _parse_ccn_result(result=result)
+
+
+def query_metric_crn(
+    session: DbSession,
+    node_id: str,
+    start_date: Optional[float] = None,
+    end_date: Optional[float] = None,
+    sort_order: Optional[str] = None,
+):
+    select_stmt = select(
+        [
+            text("item_hash"),
+            text("measured_at"),
+            text("base_latency"),
+            text("base_latency_ipv4"),
+            text("full_check_latency"),
+            text("diagnostic_vm_latency"),
+        ]
+    ).select_from(text("crn_metric_view"))
+
+    select_stmt = _build_metric_filter(
+        select_stmt=select_stmt,
+        node_id=node_id,
+        start_date=start_date,
+        end_date=end_date,
+        sort_order=sort_order,
+    )
+
+    result = session.execute(select_stmt).fetchall()
+
+    return _parse_crn_result(result=result)
