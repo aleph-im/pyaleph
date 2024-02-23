e7877d5f1de5b4b16219bfe2f2dc8c9f158118f6
Hugo Herter <git@hugoherter.com>
2024-02-23 11:25:30 +0100

    Allow developers to use nix-shell to get started (#554)
    * Fix: Developers needed to manually create a PostgreSQL and Redis databases
    
    Solution: Provide a `shell.nix` file so developers can just run `nix-shell` to start temporary PostgreSQL and Redis servers.
    
    The exit signal of the shell is `trap`-ed in order to automatically stop the services on exit.
    
    * Fix: nix-shell did not provide Python dependencies
    
    * Doc: Document usage of nix-shell
    
    * Fix: There was no CI on nix-shell
    
    * fixup! git depth
    
    * fixup! matrix run on macos too
    
    * fixup! bump action versions
    
    * fixup! CI fixes
    
    * fixup! CI fixes
    
    * fixup! CI fixes
    
    * fixup! CI fixes
    
    * WIP: Try fixing greenlet on macos
    
    * Add nix installation link
    
    Co-authored-by: Hugo Herter <git@hugoherter.com>
    
    ---------
    
    Co-authored-by: Mike Hukiewitz <70762838+MHHukiewitz@users.noreply.github.com>

---
+++ shell.nix
@@ -0,0 +1,69 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.postgresql
+    pkgs.redis
+    pkgs.kubo
+
+    pkgs.python311
+    pkgs.python311Packages.virtualenv
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.setuptools
+
+    pkgs.python311Packages.secp256k1
+    pkgs.python311Packages.fastecdsa
+    pkgs.python311Packages.greenlet
+  ];
+
+  shellHook = ''
+    echo "Setting up PostgreSQL environment..."
+    export PGDATA=$(mktemp -d)
+    PG_SOCKET_DIR=$(mktemp -d)
+    echo "Initializing database..."
+    initdb $PGDATA
+
+    echo "Starting PostgreSQL with custom socket directory..."
+    pg_ctl -D $PGDATA -o "-k $PG_SOCKET_DIR" -l logfile start
+
+    # Wait a bit for the server to start
+    sleep 1
+
+    # Create the 'aleph' role and a database
+    createuser -h $PG_SOCKET_DIR aleph
+    createdb -h $PG_SOCKET_DIR aleph -O aleph
+
+    # Create a temporary directory for Redis
+    export REDIS_DATA_DIR=$(mktemp -d)
+    redis-server --daemonize yes --dir $REDIS_DATA_DIR --bind 127.0.0.1 --port 6379
+    echo "Redis server started. Data directory is $REDIS_DATA_DIR"
+
+    echo "Starting IPFS Kubo..."
+    export IPFS_PATH=$(mktemp -d)
+    ipfs init
+    ipfs daemon &
+    echo "IPFS Kubo started. Data directory is $IPFS_PATH"
+
+    echo
+    echo "PostgreSQL started. Data directory is $PGDATA, Socket directory is $PG_SOCKET_DIR"
+    echo "Redis started. Data directory is $REDIS_DATA_DIR"
+    echo "Use 'psql -h $PG_SOCKET_DIR' to connect to the database."
+    echo "Use 'redis-cli -p 6379' to connect to the Redis server."
+    echo "To stop PostgreSQL: 'pg_ctl -D $PGDATA stop'"
+    echo "To manually stop Redis: 'redis-cli -p 6379 shutdown'"
+
+    # Trap the EXIT signal to stop services when exiting the shell
+    trap 'echo "Stopping PostgreSQL..."; pg_ctl -D "$PGDATA" stop; echo "Stopping Redis..."; redis-cli -p 6379 shutdown; echo "Stopping IPFS Kubo..."; ipfs shutdown; deactivate' EXIT
+
+    # Create a virtual environment in the current directory if it doesn't exist
+    if [ ! -d "venv" ]; then
+      python3 -m virtualenv venv
+    fi
+
+    # Install the required Python packages
+    ./venv/bin/pip install -e .\[testing\]
+
+    # Activate the virtual environment
+    source venv/bin/activate
+  '';
+}
