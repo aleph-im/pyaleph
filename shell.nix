8eae01e004d8ca9c1e5c4fc6747335e310f64686
Hugo Herter <git@hugoherter.com>
2024-05-03 17:33:46 +0200

    Fix: nix-shell did not launch RabbitMQ, required to run pyaleph

--- shell.nix
+++ shell.nix
@@ -5,6 +5,7 @@ pkgs.mkShell {
     pkgs.postgresql
     pkgs.redis
     pkgs.kubo
+    pkgs.rabbitmq-server
 
     pkgs.python311
     pkgs.python311Packages.virtualenv
@@ -44,6 +45,23 @@ pkgs.mkShell {
     ipfs daemon &
     echo "IPFS Kubo started. Data directory is $IPFS_PATH"
 
+    echo "Starting RabbitMQ..."
+    export RABBITMQ_MNESIA_BASE=$(mktemp -d)
+    export RABBITMQ_LOG_BASE=$(mktemp -d)
+    export RABBITMQ_CONFIG_FILE=$(mktemp)
+    export RABBITMQ_ENABLED_PLUGINS_FILE=$(mktemp)
+    echo "[
+      {rabbit, [
+        {mnesia_base, \"$RABBITMQ_MNESIA_BASE\"},
+        {log_base, \"$RABBITMQ_LOG_BASE\"}
+      ]}
+    ]." > $RABBITMQ_CONFIG_FILE.config
+    echo "[rabbitmq_management,rabbitmq_management_agent,rabbitmq_shovel]." > $RABBITMQ_ENABLED_PLUGINS_FILE
+    rabbitmq-server -detached
+    sleep 5 # Wait for RabbitMQ server to start fully before enabling plugins
+    echo "RabbitMQ started. Data directory is $RABBITMQ_MNESIA_BASE, Logs directory is $RABBITMQ_LOG_BASE"
+    echo "RabbitMQ Management UI enabled. Access it at http://localhost:15672/"
+
     echo
     echo "PostgreSQL started. Data directory is $PGDATA, Socket directory is $PG_SOCKET_DIR"
     echo "Redis started. Data directory is $REDIS_DATA_DIR"
@@ -51,9 +69,10 @@ pkgs.mkShell {
     echo "Use 'redis-cli -p 6379' to connect to the Redis server."
     echo "To stop PostgreSQL: 'pg_ctl -D $PGDATA stop'"
     echo "To manually stop Redis: 'redis-cli -p 6379 shutdown'"
+    echo "To stop RabbitMQ: 'rabbitmqctl stop'"
 
     # Trap the EXIT signal to stop services when exiting the shell
-    trap 'echo "Stopping PostgreSQL..."; pg_ctl -D "$PGDATA" stop; echo "Stopping Redis..."; redis-cli -p 6379 shutdown; echo "Stopping IPFS Kubo..."; ipfs shutdown; deactivate' EXIT
+    trap 'echo "Stopping PostgreSQL..."; pg_ctl -D "$PGDATA" stop; echo "Stopping Redis..."; redis-cli -p 6379 shutdown; echo "Stopping IPFS Kubo..."; ipfs shutdown; echo "Stopping RabbitMQ..."; rabbitmqctl stop; deactivate' EXIT
 
     # Create a virtual environment in the current directory if it doesn't exist
     if [ ! -d "venv" ]; then
