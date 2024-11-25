cf72a788bf948cf04fe8767f67548ff7f00a56c2
1yam <40899431+1yam@users.noreply.github.com>
2024-11-25 19:27:09 +0100

    Feature: details and chain filter on balance endpoint (#633)
    * Feature: Add balance details per account
    * Feature: Add new endpoint to return all balances
    
    ---------
    
    Co-authored-by: Laurent Peuch <cortex@worlddomination.be>
    Co-authored-by: aliel <ali.elb@gmail.com>

--- src/aleph/schemas/api/accounts.py
+++ src/aleph/schemas/api/accounts.py
@@ -1,17 +1,25 @@
 import datetime as dt
 from decimal import Decimal
-from typing import List
+from typing import Dict, List, Optional
 
-from pydantic import BaseModel, Field
+from aleph_message.models import Chain
+from pydantic import BaseModel, Field, validator
 
 from aleph.types.files import FileType
 from aleph.types.sort_order import SortOrder
-from aleph.web.controllers.utils import DEFAULT_PAGE
+from aleph.web.controllers.utils import DEFAULT_PAGE, LIST_FIELD_SEPARATOR
+
+
+class GetAccountQueryParams(BaseModel):
+    chain: Optional[Chain] = Field(
+        default=None, description="Get Balance on a specific EVM Chain"
+    )
 
 
 class GetAccountBalanceResponse(BaseModel):
     address: str
     balance: Decimal
+    details: Optional[Dict[str, Decimal]]
     locked_amount: Decimal
 
 
@@ -29,6 +37,36 @@ class GetAccountFilesQueryParams(BaseModel):
         description="Order in which files should be listed: "
         "-1 means most recent messages first, 1 means older messages first.",
     )
+
+
+class GetBalancesChainsQueryParams(BaseModel):
+    chains: Optional[List[Chain]] = Field(
+        default=None, description="Accepted values for the 'chain' field."
+    )
+    pagination: int = Field(
+        default=100,
+        ge=0,
+        description="Maximum number of files to return. Specifying 0 removes this limit.",
+    )
+    page: int = Field(
+        default=DEFAULT_PAGE, ge=1, description="Offset in pages. Starts at 1."
+    )
+    min_balance: int = Field(default=0, ge=1, description="Minimum Balance needed")
+
+    @validator("chains", pre=True)
+    def split_str(cls, v):
+        if isinstance(v, str):
+            return v.split(LIST_FIELD_SEPARATOR)
+        return v
+
+
+class AddressBalanceResponse(BaseModel):
+    class Config:
+        orm_mode = True
+
+    address: str
+    balance: str
+    chain: Chain
 
 
 class GetAccountFilesResponseItem(BaseModel):
