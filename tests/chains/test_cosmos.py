bed99faedb8bc8dae43c38db2311d4fa5a561cd0
aliel <ali.elb@gmail.com>
2025-01-22 17:54:26 +0100

    fix COSMOS

--- tests/chains/test_cosmos.py
+++ tests/chains/test_cosmos.py
@@ -1,77 +1,74 @@
 import json
+from hashlib import sha256
+from urllib.parse import unquote
 
 import pytest
+from pydantic.tools import parse_obj_as
 
 from aleph.chains.cosmos import CosmosConnector
+from aleph.schemas.pending_messages import PendingPostMessage
 
-TEST_MESSAGE = '{"chain": "CSDK", "channel": "TEST", "sender": "cosmos1rq3rcux05yftlh307gw8khh6xj43nv40mq27f5", "type": "AGGREGATE", "time": 1601997899.1849918, "item_content": "{\\"key\\":\\"test\\",\\"address\\":\\"cosmos1rq3rcux05yftlh307gw8khh6xj43nv40mq27f5\\",\\"content\\":{\\"a\\":1},\\"time\\":1601997899.1841497}", "item_hash": "248863f4eae7e31dfa33dd323af641550237c4ce90160aa1d96dd821a1b73221", "signature": "{\\"signature\\": \\"bbbdzrijw3i4eYoWhv6UI8Yqye480LOkJuNwkEp3S0sEshS9L+tQlbJSnfLDBwkC4VDh81uwhgmt57PEZws2cw==\\", \\"pub_key\\": {\\"type\\": \\"tendermint/PubKeySecp256k1\\", \\"value\\": \\"Ao3Ur9TXc/FktDebp9SnNCZWQaki/dq5G4GbiJH4aiu+\\"}, \\"account_number\\": \\"0\\", \\"sequence\\": \\"0\\"}", "content": {"key": "test", "address": "cosmos1rq3rcux05yftlh307gw8khh6xj43nv40mq27f5", "content": {"a": 1}, "time": 1601997899.1841497}}'
+TEST_MESSAGE = '{"time": 1737558660.738481, "type": "POST", "chain": "CSDK", "origin": "p2p", "sender": "cosmos1pjekm3szchhl7p2jr39hvzpwy78r5hztqdqasd", "channel": "ALEPH-CLOUDSOLUTIONS", "content": {"time": 1737558660.737648, "type": "test", "address": "cosmos1pjekm3szchhl7p2jr39hvzpwy78r5hztqdqasd", "content": {"body": "my content"}}, "item_hash": "9c7136fe3598b940158b5f0bb5c313e9152b53412db7649618003f6807739684", "item_type": "inline", "signature": {"signature": "vlrNQEokZ0bJu6WuTUH7lwaXQb0iSdLbAfuzGyB3Pp15NzFuKtNw6Fw8d4LOcV/zaVnioWMTeNxaf3Cak99ZiA==", "pub_key": {"type": "tendermint/PubKeySecp256k1", "value": "AnswvteOeBxaZVk+PAp6KZ5RSoNjAQgqSaHcfCMpjebu"}, "account_number": "0", "sequence": "0"}, "item_content":{"address":"cosmos1pjekm3szchhl7p2jr39hvzpwy78r5hztqdqasd","time":1737558660.737648,"content":{"body":"my content"},"type":"test"}}'
 
 
-@pytest.mark.skip("TODO: the verification of the signature fails, investigate why.")
+@pytest.fixture
+def cosmos_message() -> PendingPostMessage:
+    message = json.loads(unquote(TEST_MESSAGE))
+    message["signature"] = json.dumps(message["signature"])
+    message["item_content"] = json.dumps(message["item_content"], separators=(",", ":"))
+    return parse_obj_as(PendingPostMessage, message)
+
+
 @pytest.mark.asyncio
-async def test_verify_signature_real():
-    message = json.loads(TEST_MESSAGE)
+async def test_verify_signature_real(cosmos_message: PendingPostMessage):
     connector = CosmosConnector()
-    result = await connector.verify_signature(message)
-    assert result
+    assert await connector.verify_signature(message=cosmos_message)
 
 
-@pytest.mark.skip(
-    "TODO: Cosmos tests are broken, update them to use the message models."
-)
 @pytest.mark.asyncio
 async def test_verify_signature_bad_json():
     connector = CosmosConnector()
-    result = await connector.verify_signature(
+    message = parse_obj_as(
+        PendingPostMessage,
         {
-            "chain": "CHAIN",
+            "chain": "CSDK",
+            "time": 1737558660.737648,
             "sender": "SENDER",
-            "type": "TYPE",
-            "item_hash": "ITEM_HASH",
+            "type": "POST",
+            "item_hash": sha256("ITEM_HASH".encode()).hexdigest(),
             "signature": "baba",
-        }
+        },
     )
+    result = await connector.verify_signature(message)
     assert result is False
 
 
-@pytest.mark.skip(
-    "TODO: Cosmos tests are broken, update them to use the message models."
-)
 @pytest.mark.asyncio
-async def test_verify_signature_no_data():
+async def test_verify_signature_no_data(cosmos_message: PendingPostMessage):
     connector = CosmosConnector()
 
-    message = json.loads(TEST_MESSAGE)
-    signature = json.loads(message["signature"])
+    signature = json.loads(str(cosmos_message.signature))
     del signature["signature"]
-    message["signature"] = json.dumps(signature)
-    result = await connector.verify_signature(message)
+    cosmos_message.signature = json.dumps(signature)
+    result = await connector.verify_signature(cosmos_message)
     assert result is False
 
 
-@pytest.mark.skip(
-    "TODO: Cosmos tests are broken, update them to use the message models."
-)
 @pytest.mark.asyncio
-async def test_verify_signature_bad_data():
+async def test_verify_signature_bad_data(cosmos_message: PendingPostMessage):
     connector = CosmosConnector()
 
-    message = json.loads(TEST_MESSAGE)
-    signature = json.loads(message["signature"])
-    signature["signature"] = signature["signature"] + "1"
-    message["signature"] = json.dumps(signature)
-    result = await connector.verify_signature(message)
+    signature = json.loads(str(cosmos_message.signature))
+    signature["signature"] = "1" + signature["signature"]
+    cosmos_message.signature = json.dumps(signature)
+    result = await connector.verify_signature(cosmos_message)
     assert result is False
 
 
-@pytest.mark.skip(
-    "TODO: Cosmos tests are broken, update them to use the message models."
-)
 @pytest.mark.asyncio
-async def test_verify_signature_bad_address():
+async def test_verify_signature_bad_address(cosmos_message: PendingPostMessage):
     connector = CosmosConnector()
 
-    message = json.loads(TEST_MESSAGE)
-    message["sender"] = "cosmos1rq3rcux05yftlh307gw8khh6xj43nv40aq27f5"
-    result = await connector.verify_signature(message)
+    cosmos_message.sender = "cosmos1rq3rcux05yftlh307gw8khh6xj43nv40aq27f5"
+    result = await connector.verify_signature(cosmos_message)
     assert result is False
