76bbdf80bedaaaaf89b6c6720e92e02d6b0991d5
amalcaraz <amalcaraz89@gmail.com>
2025-01-29 18:27:41 +0100

    chore: fix lint issues

--- tests/message_processing/test_process_forgotten_messages.py
+++ tests/message_processing/test_process_forgotten_messages.py
@@ -1,17 +1,15 @@
 import datetime as dt
 from typing import cast
-from aleph.db.models.messages import ForgottenMessageDb, MessageDb
+
 import pytest
 from configmanager import Config
 
 from aleph.db.models import PendingMessageDb
+from aleph.db.models.messages import ForgottenMessageDb, MessageDb
 from aleph.handlers.message_handler import MessageHandler
 from aleph.storage import StorageService
-from aleph.chains.signature_verifier import SignatureVerifier
-from aleph.toolkit.timestamp import utc_now
 from aleph.types.db_session import DbSessionFactory
-from aleph.types.message_status import MessageOrigin
-from sqlalchemy.orm import selectinload
+
 from .load_fixtures import load_fixture_message_list
 
 
@@ -23,14 +21,19 @@ async def test_duplicated_forgotten_message(
     test_storage_service: StorageService,
 ):
     signature_verifier = mocker.AsyncMock()
-    
+
     messages = load_fixture_message_list("test-data-forgotten-messages.json")
 
-    m1 = PendingMessageDb.from_message_dict(messages[0], fetched=True, reception_time=dt.datetime(2025, 1, 1))
-    m2 = PendingMessageDb.from_message_dict(messages[1], fetched=True, reception_time=dt.datetime(2025, 1, 2))
-    m3 = PendingMessageDb.from_message_dict(messages[2], fetched=True, reception_time=dt.datetime(2025, 1, 3))
+    m1 = PendingMessageDb.from_message_dict(
+        messages[0], fetched=True, reception_time=dt.datetime(2025, 1, 1)
+    )
+    m2 = PendingMessageDb.from_message_dict(
+        messages[1], fetched=True, reception_time=dt.datetime(2025, 1, 2)
+    )
+    m3 = PendingMessageDb.from_message_dict(
+        messages[2], fetched=True, reception_time=dt.datetime(2025, 1, 3)
+    )
     post_hash = m1.item_hash
-
 
     message_handler = MessageHandler(
         signature_verifier=signature_verifier,
@@ -46,7 +49,10 @@ async def test_duplicated_forgotten_message(
         )
         assert test1
 
-        res1 = cast(MessageDb, session.query(MessageDb).where(MessageDb.item_hash == post_hash).first())
+        res1 = cast(
+            MessageDb,
+            session.query(MessageDb).where(MessageDb.item_hash == post_hash).first(),
+        )
         assert res1.item_hash == post_hash
 
         # 2) process forget message
@@ -56,7 +62,10 @@ async def test_duplicated_forgotten_message(
         )
         assert test2
 
-        res2 = cast(MessageDb, session.query(MessageDb).where(MessageDb.item_hash == post_hash).first())
+        res2 = cast(
+            MessageDb,
+            session.query(MessageDb).where(MessageDb.item_hash == post_hash).first(),
+        )
         assert res2 is None
 
         # 3) process post message confirmation (discarding it)
@@ -66,9 +75,16 @@ async def test_duplicated_forgotten_message(
         )
         assert test3
 
-        res3 = cast(MessageDb, session.query(MessageDb).where(MessageDb.item_hash == post_hash).first())
+        res3 = cast(
+            MessageDb,
+            session.query(MessageDb).where(MessageDb.item_hash == post_hash).first(),
+        )
         assert res3 is None
 
-        res4 = cast(ForgottenMessageDb, session.query(ForgottenMessageDb).where(ForgottenMessageDb.item_hash == post_hash).first())
+        res4 = cast(
+            ForgottenMessageDb,
+            session.query(ForgottenMessageDb)
+            .where(ForgottenMessageDb.item_hash == post_hash)
+            .first(),
+        )
         assert res4
-
