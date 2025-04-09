f7f9cfd3e1f0c4c451d3c133b6d8632ebde07de4
1yam <lyam.gomes@epitech.eu>
2025-04-09 10:49:19 +0000

    fix: remove debug print in format_message

--- src/aleph/schemas/api/messages.py
+++ src/aleph/schemas/api/messages.py
@@ -128,12 +128,7 @@ AlephMessage = Annotated[
 def format_message(message: MessageDb) -> AlephMessage:
     message_type = message.type
 
-    print(f"message_type: {message_type}")
-    print(f"message: {message.to_dict()}")
-
     message_cls = MESSAGE_CLS_DICT[message_type]
-
-    print(f"message_cls: {message_cls}")
 
     return message_cls.model_validate(message)
 
