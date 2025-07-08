d0a76ea0d9fe9ed5ea4b1a51dc968ce6636cec85
1yam <lyam.gomes@epitech.eu>
2025-07-08 14:18:39 +0200

    fix: filesystem_engine to use aiofiles to avoid I/O Blocking

--- src/aleph/services/storage/fileystem_engine.py
+++ src/aleph/services/storage/fileystem_engine.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from typing import Optional, Union
 
+import aiofiles
+import aiofiles.ospath
+
 from .engine import StorageEngine
 
 
@@ -16,19 +19,26 @@ class FileSystemStorageEngine(StorageEngine):
     async def read(self, filename: str) -> Optional[bytes]:
         file_path = self.folder / filename
 
-        if not file_path.is_file():
+        if not await aiofiles.ospath.isfile(file_path):
             return None
-
-        return file_path.read_bytes()
+        async with aiofiles.open(file_path, "rb") as f:
+            return await f.read()
 
     async def write(self, filename: str, content: bytes):
         file_path = self.folder / filename
-        file_path.write_bytes(content)
+        async with aiofiles.open(file_path, "wb") as f:
+            await f.write(content)
 
     async def delete(self, filename: str):
         file_path = self.folder / filename
-        file_path.unlink(missing_ok=True)
+        async_unlink = aiofiles.ospath.wrap(
+            Path.unlink
+        )  # We manually warp unlink (not handle by aiofiles)
+
+        await async_unlink(async_unlink(file_path, missing_ok=True))
 
     async def exists(self, filename: str) -> bool:
         file_path = self.folder / filename
-        return file_path.exists()
+        return await aiofiles.ospath.exists(
+            file_path
+        )  # This func warp .exist func into async
