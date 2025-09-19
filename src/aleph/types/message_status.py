34a1f38896d93a1d9bf8356248f83a0c216a32d0
amalcaraz <amalcaraz89@gmail.com>
2025-09-19 11:22:19 +0000

    fix: pending_message queue getting stuck due to bad error handling

--- src/aleph/types/message_status.py
+++ src/aleph/types/message_status.py
@@ -169,16 +169,6 @@ class FileNotFoundException(RetryMessageException):
         super().__init__(f"File not found: {file_hash}")
 
 
-class FileContentNotFoundException(RetryMessageException):
-    """
-    A file required to process the message could not be found, locally and/or
-    on the network.
-    """
-
-    def __init__(self, file_hash: str):
-        super().__init__(f"File content not found: {file_hash}")
-
-
 class MessageContentUnavailable(FileNotFoundException):
     """
     The message content is not available at the moment (storage/IPFS item types).
@@ -188,14 +178,6 @@ class MessageContentUnavailable(FileNotFoundException):
 
 
 class FileUnavailable(FileNotFoundException):
-    """
-    A file pointed to by the message is not available at the moment.
-    """
-
-    error_code = ErrorCode.FILE_UNAVAILABLE
-
-
-class FileContentUnavailable(FileContentNotFoundException):
     """
     A file pointed to by the message is not available at the moment.
     """
