fcdcc467308a5a965f9f9c8d232e7db42c3ffacd
Olivier Desenfans <desenfans.olivier@gmail.com>
2025-10-20 21:45:15 +0200

    fix: only attempt to re-dial to peers seen less than a day ago
    Node operators are experiencing issues on Hetzner where nodes attempt to
    dial old nodes (last active 2 years ago). The likely cause is the IPFS
    reconnection job looping over these nodes non-stop without filtering.
    
    Added a filter that only returns nodes seen after a specific datetime
    and a config value to specify the accepted period. By default, the node
    will attempt to reconnect to other peers seen at most a day ago.

--- src/aleph/db/accessors/peers.py
+++ src/aleph/db/accessors/peers.py
@@ -11,9 +11,29 @@ from ..models.peers import PeerDb, PeerType
 
 
 def get_all_addresses_by_peer_type(
-    session: DbSession, peer_type: PeerType
+    session: DbSession, peer_type: PeerType, last_seen: Optional[dt.datetime] = None,
 ) -> Sequence[str]:
+    """
+    Fetches all peer addresses filtered by peer type and optionally by the last_seen
+    timestamp. This function retrieves addresses for peers of a specific type from
+    the database. If a `last_seen` timestamp is provided, only peers with a `last_seen`
+    timestamp greater than or equal to the provided value are considered.
+
+    Arguments:
+        session (DbSession): Database session for querying data.
+        peer_type (PeerType): Type of peer to filter the addresses.
+        last_seen (Optional[datetime.datetime], optional): Timestamp to filter peers
+            last seen after or equal to this value. Defaults to None.
+
+    Returns:
+        Sequence[str]: List of addresses corresponding to the filtered peer type and
+        optional last_seen timestamp.
+    """
+
     select_peers_stmt = select(PeerDb.address).where(PeerDb.peer_type == peer_type)
+
+    if last_seen is not None:
+        select_peers_stmt = select_peers_stmt.where(PeerDb.last_seen >= last_seen)
 
     addresses = session.execute(select_peers_stmt)
     return addresses.scalars().all()
