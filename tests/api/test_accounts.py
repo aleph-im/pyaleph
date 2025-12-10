cd8f25e7cee0d6619687bab2458f18d06101661e
gmolki <gerardmo98@gmail.com>
2025-12-10 19:02:28 +0100

    feat: address channels endpoint

--- tests/api/test_accounts.py
+++ tests/api/test_accounts.py
@@ -12,6 +12,7 @@ from aleph.types.db_session import DbSessionFactory
 from aleph.types.message_status import MessageStatus
 
 ACCOUNT_POST_TYPES_URI = "/api/v0/addresses/{address}/post_types"
+ACCOUNT_CHANNELS_URI = "/api/v0/addresses/{address}/channels"
 TEST_ADDRESS = "0x9319Ad3B7A8E0eE24f2E639c40D8eD124C5520Ba"
 
 
@@ -276,4 +277,321 @@ async def test_get_account_post_types_single_type(
 
     assert data["address"] == address
     assert data["post_types"] == ["single"]
+
+
+@pytest.fixture
+def fixture_messages_with_channels(
+    session_factory: DbSessionFactory,
+) -> List[MessageDb]:
+    """Create messages with different channels for testing."""
+    now = utc_now()
+    messages = [
+        MessageDb(
+            item_hash="channel_hash1",
+            chain=Chain.ETH,
+            sender=TEST_ADDRESS,
+            signature="0x" + "0" * 128,
+            item_type=ItemType.inline,
+            type=MessageType.post,
+            item_content='{"address":"' + TEST_ADDRESS + '","time":1652126646.5,"type":"blog","content":{}}',
+            content={
+                "address": TEST_ADDRESS,
+                "time": 1652126646.5,
+                "type": "blog",
+                "content": {},
+            },
+            size=100,
+            time=now,
+            channel=Channel("channel1"),
+        ),
+        MessageDb(
+            item_hash="channel_hash2",
+            chain=Chain.ETH,
+            sender=TEST_ADDRESS,
+            signature="0x" + "0" * 128,
+            item_type=ItemType.inline,
+            type=MessageType.post,
+            item_content='{"address":"' + TEST_ADDRESS + '","time":1652126647.5,"type":"blog","content":{}}',
+            content={
+                "address": TEST_ADDRESS,
+                "time": 1652126647.5,
+                "type": "blog",
+                "content": {},
+            },
+            size=100,
+            time=now + dt.timedelta(seconds=1),
+            channel=Channel("channel1"),
+        ),
+        MessageDb(
+            item_hash="channel_hash3",
+            chain=Chain.ETH,
+            sender=TEST_ADDRESS,
+            signature="0x" + "0" * 128,
+            item_type=ItemType.inline,
+            type=MessageType.aggregate,
+            item_content='{"address":"' + TEST_ADDRESS + '","key":"test","time":1652126648.5,"content":{}}',
+            content={
+                "address": TEST_ADDRESS,
+                "key": "test",
+                "time": 1652126648.5,
+                "content": {},
+            },
+            size=100,
+            time=now + dt.timedelta(seconds=2),
+            channel=Channel("channel2"),
+        ),
+        MessageDb(
+            item_hash="channel_hash4",
+            chain=Chain.ETH,
+            sender=TEST_ADDRESS,
+            signature="0x" + "0" * 128,
+            item_type=ItemType.inline,
+            type=MessageType.store,
+            item_content='{"address":"' + TEST_ADDRESS + '","time":1652126649.5,"item_hash":"hash123","item_type":"ipfs"}',
+            content={
+                "address": TEST_ADDRESS,
+                "time": 1652126649.5,
+                "item_hash": "hash123",
+                "item_type": "ipfs",
+            },
+            size=100,
+            time=now + dt.timedelta(seconds=3),
+            channel=Channel("channel3"),
+        ),
+        # Message with null channel should be filtered out
+        MessageDb(
+            item_hash="channel_hash5",
+            chain=Chain.ETH,
+            sender=TEST_ADDRESS,
+            signature="0x" + "0" * 128,
+            item_type=ItemType.inline,
+            type=MessageType.post,
+            item_content='{"address":"' + TEST_ADDRESS + '","time":1652126650.5,"type":"blog","content":{}}',
+            content={
+                "address": TEST_ADDRESS,
+                "time": 1652126650.5,
+                "type": "blog",
+                "content": {},
+            },
+            size=100,
+            time=now + dt.timedelta(seconds=4),
+            channel=None,
+        ),
+        # Message from different address should be filtered out
+        MessageDb(
+            item_hash="channel_hash6",
+            chain=Chain.ETH,
+            sender="0xDifferentAddress",
+            signature="0x" + "0" * 128,
+            item_type=ItemType.inline,
+            type=MessageType.post,
+            item_content='{"address":"0xDifferentAddress","time":1652126651.5,"type":"blog","content":{}}',
+            content={
+                "address": "0xDifferentAddress",
+                "time": 1652126651.5,
+                "type": "blog",
+                "content": {},
+            },
+            size=100,
+            time=now + dt.timedelta(seconds=5),
+            channel=Channel("other_channel"),
+        ),
+    ]
+
+    message_statuses = [
+        MessageStatusDb(
+            item_hash=msg.item_hash,
+            status=MessageStatus.PROCESSED,
+            reception_time=utc_now(),
+        )
+        for msg in messages
+    ]
+
+    with session_factory() as session:
+        session.add_all(messages)
+        session.add_all(message_statuses)
+        session.commit()
+
+    return messages
+
+
+@pytest.mark.asyncio
+async def test_get_account_channels(
+    ccn_api_client,
+    fixture_messages_with_channels: List[MessageDb],
+):
+    """Test getting channels for an address with multiple messages."""
+    uri = ACCOUNT_CHANNELS_URI.format(address=TEST_ADDRESS)
+    response = await ccn_api_client.get(uri)
+
+    assert response.status == 200, await response.text()
+    data = await response.json()
+
+    assert data["address"] == TEST_ADDRESS
+    assert "channels" in data
+    # Should return distinct channels: channel1, channel2, channel3 (sorted)
+    assert set(data["channels"]) == {"channel1", "channel2", "channel3"}
+    assert len(data["channels"]) == 3
+    # Should be sorted
+    assert data["channels"] == sorted(data["channels"])
+
+
+@pytest.mark.asyncio
+async def test_get_account_channels_empty(
+    ccn_api_client,
+):
+    """Test getting channels for an address with no messages."""
+    uri = ACCOUNT_CHANNELS_URI.format(address=TEST_ADDRESS)
+    response = await ccn_api_client.get(uri)
+
+    assert response.status == 200, await response.text()
+    data = await response.json()
+
+    assert data["address"] == TEST_ADDRESS
+    assert data["channels"] == []
+
+
+@pytest.mark.asyncio
+async def test_get_account_channels_only_null(
+    ccn_api_client,
+    session_factory: DbSessionFactory,
+):
+    """Test getting channels when address has only messages with null channels."""
+    address = "0xNullChannelsAddress"
+    now = utc_now()
+
+    messages = [
+        MessageDb(
+            item_hash="null1",
+            chain=Chain.ETH,
+            sender=address,
+            signature="0x" + "0" * 128,
+            item_type=ItemType.inline,
+            type=MessageType.post,
+            item_content=f'{{"address":"{address}","time":1652126646.5,"type":"blog","content":{{}}}}',
+            content={
+                "address": address,
+                "time": 1652126646.5,
+                "type": "blog",
+                "content": {},
+            },
+            size=100,
+            time=now,
+            channel=None,
+        ),
+    ]
+
+    message_statuses = [
+        MessageStatusDb(
+            item_hash=msg.item_hash,
+            status=MessageStatus.PROCESSED,
+            reception_time=utc_now(),
+        )
+        for msg in messages
+    ]
+
+    with session_factory() as session:
+        session.add_all(messages)
+        session.add_all(message_statuses)
+        session.commit()
+
+    uri = ACCOUNT_CHANNELS_URI.format(address=address)
+    response = await ccn_api_client.get(uri)
+
+    assert response.status == 200, await response.text()
+    data = await response.json()
+
+    assert data["address"] == address
+    assert data["channels"] == []
+
+
+@pytest.mark.asyncio
+async def test_get_account_channels_different_address(
+    ccn_api_client,
+    fixture_messages_with_channels: List[MessageDb],
+):
+    """Test getting channels for a different address."""
+    different_address = "0xDifferentAddress"
+    uri = ACCOUNT_CHANNELS_URI.format(address=different_address)
+    response = await ccn_api_client.get(uri)
+
+    assert response.status == 200, await response.text()
+    data = await response.json()
+
+    assert data["address"] == different_address
+    assert "channels" in data
+    assert set(data["channels"]) == {"other_channel"}
+    assert len(data["channels"]) == 1
+
+
+@pytest.mark.asyncio
+async def test_get_account_channels_single_channel(
+    ccn_api_client,
+    session_factory: DbSessionFactory,
+):
+    """Test getting channels when address has only one channel."""
+    address = "0xSingleChannelAddress"
+    now = utc_now()
+
+    messages = [
+        MessageDb(
+            item_hash="single1",
+            chain=Chain.ETH,
+            sender=address,
+            signature="0x" + "0" * 128,
+            item_type=ItemType.inline,
+            type=MessageType.post,
+            item_content=f'{{"address":"{address}","time":1652126646.5,"type":"blog","content":{{}}}}',
+            content={
+                "address": address,
+                "time": 1652126646.5,
+                "type": "blog",
+                "content": {},
+            },
+            size=100,
+            time=now,
+            channel=Channel("single_channel"),
+        ),
+        MessageDb(
+            item_hash="single2",
+            chain=Chain.ETH,
+            sender=address,
+            signature="0x" + "0" * 128,
+            item_type=ItemType.inline,
+            type=MessageType.aggregate,
+            item_content=f'{{"address":"{address}","key":"test","time":1652126647.5,"content":{{}}}}',
+            content={
+                "address": address,
+                "key": "test",
+                "time": 1652126647.5,
+                "content": {},
+            },
+            size=100,
+            time=now + dt.timedelta(seconds=1),
+            channel=Channel("single_channel"),
+        ),
+    ]
+
+    message_statuses = [
+        MessageStatusDb(
+            item_hash=msg.item_hash,
+            status=MessageStatus.PROCESSED,
+            reception_time=utc_now(),
+        )
+        for msg in messages
+    ]
+
+    with session_factory() as session:
+        session.add_all(messages)
+        session.add_all(message_statuses)
+        session.commit()
+
+    uri = ACCOUNT_CHANNELS_URI.format(address=address)
+    response = await ccn_api_client.get(uri)
+
+    assert response.status == 200, await response.text()
+    data = await response.json()
+
+    assert data["address"] == address
+    assert data["channels"] == ["single_channel"]
 
