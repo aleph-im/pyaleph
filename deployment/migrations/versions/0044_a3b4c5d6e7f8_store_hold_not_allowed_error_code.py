aec0052ed8642420633ceba8acc03e6cba3130ea
amalcaraz <amalcaraz89@gmail.com>
2026-02-10 18:53:29 +0100

    fix: tests

--- deployment/migrations/versions/0044_a3b4c5d6e7f8_store_hold_not_allowed_error_code.py
+++ deployment/migrations/versions/0044_a3b4c5d6e7f8_store_hold_not_allowed_error_code.py
@@ -1,7 +1,7 @@
 """Add STORE_HOLD_NOT_ALLOWED error code
 
 Revision ID: a3b4c5d6e7f8
-Revises: f2a3b4c5d6e7
+Revises: a1b2c3d4e5f7
 Create Date: 2026-02-09
 
 Adds error code 202 for STORE messages that attempt to use hold payment
@@ -13,7 +13,7 @@ from sqlalchemy import text
 
 # revision identifiers, used by Alembic.
 revision = "a3b4c5d6e7f8"
-down_revision = "f2a3b4c5d6e7"
+down_revision = "a1b2c3d4e5f7"
 branch_labels = None
 depends_on = None
 
